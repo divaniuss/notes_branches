98781b77aae270f53846676d6577f54a196c9efd
Dmitriy <dimasikemelka@gmail.com>
2025-02-19 11:26:05 +0200

    added class Manager in manager.py

---
+++ manager.py
@@ -0,0 +1,20 @@
+class Manager:
+    def __init__(self):
+        self.notes = []
+
+    def add(self, note):
+        self.notes.append(note)
+
+    def search(self, id):
+        for note in self.notes:
+            if note.id == id:
+                return note
+        return None
+
+    def delete(self, id):
+        self.notes = [note for note in self.notes if note.id != id]
+
+    def show_all(self):
+        print("Все заметки:")
+        for note in self.notes:
+            print(note.toString())
